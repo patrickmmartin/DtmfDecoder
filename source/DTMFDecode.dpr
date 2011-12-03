d23a951c0447f89bbe5fabeed558f8cecfe58db6
Patrick Martin <patrickmmartin@gmail.com>
2011-12-03 18:10:15 +0000

    adding initial project suite and projects

---
+++ source/DTMFDecode.dpr
@@ -0,0 +1,13 @@
+program DTMFDecode;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  DTMF in 'DTMF.pas',
+  DTMFConsts in 'DTMFConsts.pas',
+  NSPErrorHandler in 'NSPErrorHandler.pas';
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+end.
